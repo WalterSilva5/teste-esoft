026d7187fa15a9bdf7b796a4e20b4ea436670146
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-08 03:03:26 -0300

    listagem iniciada

--- app/static/app/js/produtos.js
+++ app/static/app/js/produtos.js
@@ -1,25 +1,50 @@
 function cadastrar_produto() {
+  let nome = $(`input[name='nome']`).val();
+  let estoque = $(`input[name='estoque']`).val();
+  let preco = $(`input[name='preco']`).val();
   $.ajax({
     type: "post",
     url: "http://localhost:8000/produtos/cadastrar_produto",
     data: {
-      nome: $(`input[name='nome']`).val(),
-      estoque: $(`input[name='estoque']`).val(),
-      preco: $(`input[name='preco']`).val(),
+      nome: nome,
+      estoque: estoque,
+      preco: preco,
     },
     success: function (result) {
-      $('#produto_cadastro_efetuado').removeClass("d-none");
-      $('#produto_cadastro_nao_efetuado').addClass("d-none");
-
-      $('input').val("");
+      console.log(result);
+      if (result == "OK") {
+        sucesso();
+      } else if (result == "PRODUTO_JA_CADASTRADO") {
+        mensagem = "PRODUTO JA ESTA CADASTRADO";
+      } else if (result == "ERRO_DESCONHECIDO") {
+        mensagem = result;
+      }else if(result == "ESTOQUE_NEGATIVO"){
+        mensagem = "ESTOQUE NÃO PODE SER NEGATIVO!"
+      }else if(result == "PRECO_NEGATIVO"){
+        mensagem = "PREÇO NÃO PODE SER NEGATIVO!"
+      }else {
+        erro("ERRO_DESCONHECIDO");
+      }
+      erro(mensagem);
     },
-    error: function(result){
-      $('#produto_cadastro_efetuado').addClass("d-none");
-      $('#produto_cadastro_nao_efetuado').removeClass("d-none");
-    }
+    error: function (result) {
+      erro("ERRO: "+mensagem);
+    },
   });
 }
-
-$("#fechar_cadastro_de_produto").on('click', function(){
-  $('input').val("");
+function sucesso() {
+  $("#produto_cadastro_efetuado").removeClass("d-none");
+  $("#produto_cadastro_nao_efetuado").addClass("d-none");
+  $("input").val("");
+}
+function erro(mensagem) {
+  $("#produto_cadastro_efetuado").addClass("d-none");
+  $("#produto_cadastro_nao_efetuado").removeClass("d-none");
+  $("#mensagem_erro").text(mensagem);
+}
+$("#modal_adicionar_produto").on("hidden.bs.modal", function () {
+  $("#produto_cadastro_nao_efetuado").removeClass("d-none");
+  $("#produto_cadastro_nao_efetuado").addClass("d-none");
+  $("#form_adicionar_produto").trigger("reset");
+  location.reload();
 });
--- app/views/produtos.py
+++ app/views/produtos.py
@@ -11,7 +11,9 @@ manipula_session = ManipulaSession()
 def produtos(request):
     #verifica se o usuario esta logado para poder acessar a pagina
     if manipula_session.verifica_logado(request):
-        return render(request, 'produtos.html')
+        produtos = list(Produto.objects.all().values())[:]
+        print(produtos)
+        return render(request, 'produtos.html', {"produtos": produtos})
     else:
         return render(request, 'login.html', {"mensagem": "ERRO: NECESSARIO FAZER LOGIN!"})
 
@@ -21,10 +23,18 @@ def produto_cadastrado(nome):
     
 @csrf_exempt
 def cadastrar_produto(request):
-    nome = request.POST["nome"]
+    nome = request.POST["nome"].upper()
+    estoque = float(request.POST["estoque"])
+    preco = float(request.POST["preco"])
+
+    
     if produto_cadastrado(nome):
-        return  HttpResponse(True)
+        return  HttpResponse("PRODUTO_JA_CADASTRADO")
+    elif estoque < 0:
+        return HttpResponse("ESTOQUE_NEGATIVO")
+    elif preco < 0:
+        return HttpResponse("PRECO_NEGATIVO")
     else:
-        produto = Produto(nome = request.POST["nome"].upper(), estoque = request.POST["estoque"], preco = request.POST["preco"])
+        produto = Produto(nome=nome, estoque=estoque, preco=preco)
         produto.save()
-        return HttpResponse(False)
+        return HttpResponse("OK")
